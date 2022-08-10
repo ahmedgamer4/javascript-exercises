96608801cb5fa588e79e4b1151b01ad0354f8ede
Your Name <yourname@example.com>
2022-08-10 16:53:21 -0400

    finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, times) {
+    let txt = '';
+    if (times < 0){
+        return 'ERROR';
+    }
+    for (var i = 0; i < times; i++){
+        txt += text;
+    }
+    return txt;
 };
 
 // Do not edit below this line
