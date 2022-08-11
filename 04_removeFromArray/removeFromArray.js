ce2a5ee538388c9d7d69430b197edad8988fd826
Your Name <yourname@example.com>
2022-08-11 03:55:40 -0400

    finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const list = args[0];
+    const n_list = [];
 
+    for (item of list){
+        if (!args.includes(item)){
+            n_list.push(item);
+        }
+    }
+    return n_list;
 };
 
 // Do not edit below this line
