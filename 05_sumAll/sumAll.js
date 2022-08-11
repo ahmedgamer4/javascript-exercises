1b6de5b6f56c7e9488f1dd8997569f15209f4683
Your Name <yourname@example.com>
2022-08-11 04:11:25 -0400

    finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (typeof start != "number" || typeof end != "number" || Math.sign(start) == -1 || Math.sign(end) == -1){
+        return 'ERROR';
+    }
+    else{
+        if (end < start){
+            let tmp = start;
+            start = end;
+            end = tmp;
+        }
+        for (var i = start; i <= end; i++){
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
